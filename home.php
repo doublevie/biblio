1ba5ad6b4bedaa4953dbe6ddf3a7b9e33c2699b3
fares abadou <fares.abadou@gmail.com>
2017-09-27 23:03:42 +0100

    added home with nav

--- home.php
+++ home.php
@@ -1,6 +1,36 @@
-<?php
 
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="libs/bootstrap-rtl/dist/css/bootstrap-rtl.css">
+    <link rel="stylesheet" href="libs/css/main.css">
+    <style media="screen">
 
 
- Welcome Home !
+     </style>
+  </head>
+  <body>
+<?php include 'inc/nav.php'; ?>
+
+
+<div class="container">
+<h1>welcom</h1>
+</div>
+
+
+
+
+
+
+  </body>
+  <script type="text/javascript" src="libs/jquery/dist/jquery.js">  </script>
+  <script type="text/javascript" src="libs/bootstrap/dist/js/bootstrap.js">  </script>
+
+
+
+
+ </html>
